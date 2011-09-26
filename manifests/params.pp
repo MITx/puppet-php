fc35d6aea158a2fc34512c0f600ad335a9fe1568
Thomas Sutton <me@thomas-sutton.id.au>
2011-09-26 19:57:47 +0800

    Basic support for Apache SAPI

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,16 @@
 
 class php::params {
 
-  $sapi_apache_packages = ['libapache2-mod-php5']
-  $sapi_apache_config = "/etc/php5/apache/php.ini"
-  
-  $sapi_cli_packages = ['php5-cli']
-  $sapi_cgi_packages = ['php5-cgi'] 
-  
+	$sapi_apache_packages = ['libapache2-mod-php5']
+	$sapi_apache_config = "/etc/php5/apache/php.ini"
+	$sapi_apache_config_d = "/etc/php5/apache/conf.d"
+
+	$sapi_cli_packages = ['php5-cli']
+	$sapi_cli_config = "/etc/php5/cli/php.ini"
+	$sapi_cli_config_d = "/etc/php5/cli/conf.d"
+
+	$sapi_cgi_packages = ['php5-cgi'] 
+	$sapi_cgi_config = "/etc/php5/cgi/php.ini"
+	$sapi_cgi_config_d = "/etc/php5/cgi/conf.d"
+
 }
--- manifests/sapi/apache.pp
+++ manifests/sapi/apache.pp
@@ -1,10 +1,47 @@
+#
+# Class:
+#     php::sapi::apache
+#
+# Description:
+#     Install and (fail to) configure the Apache PHP module.
+#
+class php::sapi::apache {
 
-class php::sapi::apache (
-  $packages = $php::params::sapi_apache_packages
-) inherits php::params {
+	notify { "Installing php for apache" : }
 
-  package { $packages :
-    ensure => installed,
-  }
+	include php::sapi::apache::install
+	include php::sapi::apache::configure
+
 
 }
+
+
+class php::sapi::apache::install (
+	$packages = $php::params::sapi_apache_packages
+) inherits php::params {
+
+	package { $packages :
+		ensure => installed,
+	}
+
+}
+
+class php::sapi::apache::configure (
+	$php_ini = $php::params::sapi_apache_config,
+	$php_ini_d = $php::params::sapi_apache_config_d
+) inherits php::params {
+
+
+	file { $php_ini :
+		ensure => present,
+	}
+
+	file { $php_ini_d :
+		ensure => directory,
+		recurse => true,
+	}
+
+	Class['php::sapi::apache::install'] -> Class['php::sapi::apache::configure']
+	Class['php::sapi::apache::configure'] ~> Class['apache::service']
+}
+
